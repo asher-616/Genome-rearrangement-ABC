8ab223bc42b48c19ea1aa55d8ee7a04c482c9cbe
asher-616 <asher616@gmail.com>
2021-12-20 17:00:24 +0200

    restart

--- ABC - revision/test.cpp
+++
@@ -1,9 +0,0 @@
-#include <fstream>
-#include <iostream>
-
-int main() {
-	std::fstream f;
-	f.open("test.txt", std::fstream::in);
-	std::cout << "managed to open";
-	f.close();
-}
--- ABC - revision/treeIt.cpp
+++
@@ -1,3 +0,0 @@
-#include "treeIt.h"
-
-
